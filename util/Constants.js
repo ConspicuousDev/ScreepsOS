165e6718d2d185038ede7c2cd5b5b84e36b1532e
OmniscientARK <lorenzopiuga@gmail.com>
2022-11-21 00:55:22 -0800

    Update constant values

--- util/Constants.js
+++ util/Constants.js
@@ -1,18 +1,18 @@
 module.exports = {
     STATUS_CODES: {
-        DEAD: -1,
-        OK: 0,
-        WAITING_CHILD: 1,
-        DONE: 2,
+        DEAD: "DEAD",
+        OK: "OK",
+        WAITING_CHILD: "WAITING_CHILD",
+        DONE: "DONE",
     },
     PROCESS_PRIORITIES: {
         KERNEL: 100,
         USER: 0
     },
     ROOM_OWNERSHIP:{
-        UNCLAIMED: 0,
-        OWNED: 1,
-        ENEMY: 2
+        UNCLAIMED: "UNCLAIMED",
+        OWNED: "OWNED",
+        ENEMY: "ENEMY"
     },
     COLORS: {
         FREE: "#2B59C3",
