972ed11fff4bca446806ef037be1be033a072d50
Lorenzo <Lo34673467*>
2022-11-23 03:14:38 -0800

    Created SubProcess: MoveToTarget

---
+++ processes/subroutines/MoveToTargetProcess.js
@@ -0,0 +1,12 @@
+const Process = require("../kernel/Process")
+const OSConstants = require("../util/OSConstants")
+
+class MoveToTargetProcess extends Process {
+    constructor({kernel, data, priority = 0}){
+        super(`MoveToTargetProcess-${parent}`, null, priority, OSConstants.STATUS_CODES.OK, kernel, data)
+    }
+
+    run(){
+        let creep = Game.creeps[data.creepName]
+    }
+}
